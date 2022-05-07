e19c40227b904b87adba68c64a267fe93908ea2d
Thavi <thavi@bravo.st>
2022-05-07 09:29:23 -0300

    iniciando

---
+++ contact-list.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+* Plugin Name: Contact List
+* Plugin URI: https://thavi.dev
+* Description: Insert a brief description of what your plugin does here.
+* Version: 1.0.0
+* Author: Thavi Lang
+* Author URI: https://thavi.dev
+* License: GPL2
+*/
